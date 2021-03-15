636de894bd903dcdb0d4428a5dde3e1a2e0e4993
clerked <414862098@qq.com>
2021-03-15 12:42:52 +0800

    first commit

---
+++ CONFIG.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8
+
+#ABOX配置
+class ABOX():
+    #域名或IP
+    host = ""
+    #密码
+    password = ""
+    #登陆端口(内网80)
+    loginPort = 80
+    #MQTT端口(内网1883)
+    mqttPort = 1883
+
+#MQTT配置
+class MQTT():
+    #域名或IP
+    host = ""
+    #端口
+    port = 1883
